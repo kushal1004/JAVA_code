a511de58d89eeaa355fc8604af776733f1a2c661
Kushal Singha <kushalsingha00@gmail.com>
2025-08-20 10:50:14 +0530

    changing the space complexity

--- MatrixChallenge.java
+++ MatrixChallenge.java
@@ -6,8 +6,13 @@ class subMatrix {
     public String matrixopt(String[] str) {
         int n = str.length; // row value
         int m = str[0].length(); // column value
+/* 
 
-        int[] prev = new int[m];
+// Time complexity = O(n*m)
+//Space complexity = O(m)
+
+
+ int[] prev = new int[m];
         int[] curr = new int[m];
         int maxSide = 0;
 
@@ -27,6 +32,27 @@ class subMatrix {
             prev = curr;
             curr = temp;
         }
+*/
+
+// Time complexity = O(n*m)
+//Space complexity = O(n*m)
+
+        int[][] dp = new int[n][m];
+        int maxSide = 0;
+         
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(str[i].charAt(j) == '1'){
+                    if(i==0||j==0){
+                        dp[i][j] = 1;
+                    }else{
+                        dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1])) + 1;
+                    }
+                    maxSide = Math.max(maxSide,dp[i][j]);
+                }
+            }
+        }
+        
         return String.valueOf(maxSide * maxSide);
     }
 }

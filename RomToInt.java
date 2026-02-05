d7740fca251e2d6dea154bb71fd940ab7c49056b
Kushal <kushalsingha00@gmail.com>
2026-02-06 00:34:58 +0530

    feat: CL input/output

--- RomToInt.java
+++ RomToInt.java
@@ -1,4 +1,22 @@
-;
 public class RomToInt {
-    // first do it then go for java script
+    public static void main(String[] args) {
+        // for each loop with CLI arguments
+        System.out.println("Before type change: ");
+
+        for (String str : args) {
+            System.out.println(str + 1);
+        }
+
+        System.out.println("After type change: ");
+
+        int sum = 0;
+
+        for (String str : args) {
+            sum += Integer.parseInt(str);
+
+            System.out.println(Integer.parseInt(str) + 1);
+        }
+
+        System.out.println("The sum of the numbers is: " + sum);
+    }
 }

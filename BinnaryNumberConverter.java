9b7ae295e3706669c1c07781a63f91098b895c84
Kushal <kushalsingha00@gmail.com>
2026-02-09 20:14:02 +0530

    feat: code example of array

--- BinnaryNumberConverter.java
+++ BinnaryNumberConverter.java
@@ -15,5 +15,20 @@ public class BinnaryNumberConverter {
         Converter cnv = new Converter();
         cnv.binaryToDecimal("1010");
         cnv.decimalToBinary(10);
+
+        int[][] num = new int[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                num[i][j] = (int) (Math.random() * 10);
+            }
+        }
+
+        for (int n[] : num) {
+            for (int m : n) {
+                System.out.print(m + " ");
+            }
+            System.out.println("");
+        }
     }
 }
